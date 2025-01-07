f6f7ba09385372c1aa299975175d7a0af77aa33f
Toxa <untoxa@mail.ru>
2025-01-07 22:08:16 +0300

    EXAMPLES: use JOYPAD definitions instead of direct values in the multiplayer example

--- examples/multiplayer/src/SpritePlayer2.c
+++ examples/multiplayer/src/SpritePlayer2.c
@@ -16,14 +16,14 @@ void START(void) {
 
 void UPDATE(void) {
 	// check D-Pad 1 buttons and move the player within the screen boundaries
-	if (PAD_PRESSED(1, J_LEFT)) {
+	if (PAD_PRESSED(JOYPAD1, J_LEFT)) {
 		if (THIS->x) THIS->x--;
-	} else if (PAD_PRESSED(1, J_RIGHT)) {
+	} else if (PAD_PRESSED(JOYPAD1, J_RIGHT)) {
 		if (THIS->x < (SCREEN_WIDTH - 16)) THIS->x++;
 	}
-	if (PAD_PRESSED(1, J_UP)) {
+	if (PAD_PRESSED(JOYPAD1, J_UP)) {
 		if (THIS->y) THIS->y--;
-	} else if (PAD_PRESSED(1, J_DOWN)) {
+	} else if (PAD_PRESSED(JOYPAD1, J_DOWN)) {
 		if (THIS->y < (SCREEN_HEIGHT - 16)) THIS->y++;
 	}
 }
