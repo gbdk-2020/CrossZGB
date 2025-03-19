7c3b8169c4ea9af97a8c44a40cc0b3fba9a8affc
Toxa <untoxa@mail.ru>
2025-03-19 20:14:09 +0300

    EXAMPLES: ability to toggle music on and off in the psglib example

--- examples/psglib/src/ZGBMain.c
+++ examples/psglib/src/ZGBMain.c
@@ -3,9 +3,3 @@
 
 // initialize the game state at start
 UINT8 next_state = StateGame;
-
-// we don't need tile replacement here because this example has no background
-UINT8 GetTileReplacement(UINT8* tile_ptr, UINT8* tile) {
-	tile_ptr; tile; // suppress warning
-	return 255u;    // don't replace anything
-}
