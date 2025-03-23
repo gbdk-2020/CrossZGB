8a13ffc8ffad33f34029aad7a6c7dbcf13ad2714
Toxa <untoxa@mail.ru>
2025-03-23 18:25:01 +0300

    improve windows install.bat script

--- install.bat
+++ install.bat
@@ -1,1 +1,18 @@
+@echo off
+if "%ZGB_PATH%"=="" goto :continue
+echo CrossZGB is already installed!
+goto :EOF
+
+:continue
+echo Installing CrossZGB:
+
+echo Adding path to MAKE and MSYS into the system PATH...
+setx /M PATH "%cd%\..\env\make-3.81-bin\bin;%cd%\..\env\msys\bin\;%PATH%"
+
+echo Setting ZGB_PATH to "%cd%\common"...
 setx ZGB_PATH "%cd%\common"
+
+echo Setting GBDK_HOME to "%cd%\..\gbdk"...
+setx GBDK_HOME "%cd%\..\gbdk"
+
+echo Done!
