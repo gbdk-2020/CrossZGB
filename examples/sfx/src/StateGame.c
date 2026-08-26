920fdeb23b0d19728410483327db0953c6dcb38a
Toxa <untoxa@mail.ru>
2026-08-26 12:15:12 +0300

    EXAMPLES: Comments in code for the SFX example

--- examples/sfx/src/StateGame.c
+++ examples/sfx/src/StateGame.c
@@ -6,9 +6,14 @@
 #include "Sound.h"
 #include "Keys.h"
 
+// declare the SFX assets
+
+// SFX in the FXHammer format (same file for the each platform)
 DECLARE_SFX(sfx_fxhammer);
+// SFX in the VGM format (different files for the different platforms under the same name)
 DECLARE_SFX(sfx_vgm);
 #if defined(NINTENDO)
+// SFX in the WAV format (feature is available only on the Game Boy)
 DECLARE_SFX(sfx_wav);
 #endif
 
@@ -37,10 +42,14 @@ void START(void) {
 }
 
 void UPDATE(void) {
+	// Play FXHammer - the cross-platform format (with few differences in sounding between the GB/SN7, but very similar)
 	if (KEY_TICKED(J_A)) ExecuteSFX(BANK(sfx_fxhammer), sfx_fxhammer, SFX_MUTE_MASK(sfx_fxhammer), SFX_PRIORITY_NORMAL);
+	// play VGM - GB and SN7 assets MUST be different, because sound chips are not compatible
 	if (KEY_TICKED(J_B)) ExecuteSFX(BANK(sfx_vgm),      sfx_vgm,      SFX_MUTE_MASK(sfx_vgm),      SFX_PRIORITY_NORMAL);
 #if defined(NINTENDO)
+	// play WAV (1920Hz mono) - only supported on the Game Boy
 	if (KEY_TICKED(J_SELECT)) ExecuteSFX(BANK(sfx_wav), sfx_wav,      SFX_MUTE_MASK(sfx_wav),      SFX_PRIORITY_NORMAL);
+	// play using legacy PlayFx() function - this is Game Boy only, obsolete and not recommended for using, but still valid
 	if (KEY_TICKED(J_START))  PlayFx(CHANNEL_1, 10, 0x2C, 0xD8, 0xF3, 0x62, 0xC7);
 #endif
 }
